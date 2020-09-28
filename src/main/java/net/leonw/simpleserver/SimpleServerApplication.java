b62615dd2aa2370f96b6ae714631412177e6c256
Leon <qaz@dds.nl>
2020-09-28 18:31:22 +0200

    Compacted code a bit. Made the mock data relations slightly more life-like. Added 404 related code.

--- src/main/java/net/leonw/simpleserver/SimpleServerApplication.java
+++ src/main/java/net/leonw/simpleserver/SimpleServerApplication.java
@@ -5,10 +5,12 @@ import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -17,49 +19,46 @@ import java.util.stream.Stream;
 
 @SpringBootApplication
 public class SimpleServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SimpleServerApplication.class, args);
     }
-
 }
 
 @Data
 @AllArgsConstructor
-class Order {
-    private String id;
-    private String data;
-}
+class Order { private String id; private List<String> orderLineIds;}
 
 @Data
 @AllArgsConstructor
 class OrderLine {
-    private String id;
-    private String data;
+    private String id, productId;
 }
 
 @Data
 @AllArgsConstructor
 class Product {
-    private String id;
-    private String data;
+    private String id, data;
 }
 
 @Slf4j
 @RestController
 class Controller {
+    private static final int WAIT_TIME_SECONDS = 2;
+    private static final List<String> ORDER_LINE_IDS = List.of("a-1", "a-2", "b-1");
+
     @GetMapping("/orders/{id}")
     public Order orderById(@PathVariable("id") String id) {
-        sleep(1);
-        return new Order(id, id);
+        sleep(WAIT_TIME_SECONDS);
+        // throw new ResponseStatusException(  HttpStatus.NOT_FOUND, "entity not found" );
+        return new Order(id, ORDER_LINE_IDS);
     }
 
     @GetMapping("/orders/")
     public List<Order> searchOrders(@RequestParam("tag") String tag) {
         if ("open".equals(tag)) {
-            sleep(1);
+            sleep(WAIT_TIME_SECONDS);
             return Stream.of("a", "b", "c")
-                    .map(id -> new Order(id, id))
+                    .map(id -> new Order(id, ORDER_LINE_IDS))
                     .collect(Collectors.toList());
         } else {
             throw new IllegalArgumentException("Tag " + tag + " is not supported. Supported is: 'open'");
@@ -68,14 +67,21 @@ class Controller {
 
     @GetMapping("/orderlines/{id}")
     public OrderLine orderLineById(@PathVariable("id") String id) {
-        sleep(1);
+        sleep(WAIT_TIME_SECONDS);
         return new OrderLine(id, id);
     }
 
     @GetMapping("/products/{id}")
     public Product productById(@PathVariable("id") String id) {
-        sleep(1);
+        sleep(WAIT_TIME_SECONDS);
         return new Product(id, id);
+    }
+
+    @GetMapping("/notFound")
+    public void notFound() {
+        throw new ResponseStatusException(
+                HttpStatus.NOT_FOUND, "entity not found"
+        );
     }
 
     private void sleep(long seconds) {
